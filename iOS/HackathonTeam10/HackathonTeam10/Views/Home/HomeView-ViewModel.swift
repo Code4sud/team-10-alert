90d1ec6b0feecf9c0a8ce7bb7f87fb7da743c331
franck walter <fwalter@dev-id.fr>
2024-11-07 13:26:43 +0100

    feat: nav

---
+++ iOS/HackathonTeam10/HackathonTeam10/Views/Home/HomeView-ViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView-ViewModel.swift
+//  HackathonTeam10
+//
+//  Created by Franck Walter on 07/11/2024.
+//
+
+import SwiftUI
+
+extension HomeView {
+    @Observable
+    class ViewModel {
+
+        
+        init() {
+        }
+        
+    }
+}
---
+++ iOS/HackathonTeam10/HackathonTeam10/Views/SplashView/SplashView .swift
@@ -0,0 +1,18 @@
+
+//
+//  SplashView .swift
+//  HackathonTeam10
+//
+//  Created by Franck Walter on 07/11/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Color.black.frame(width: .infinity, height: .infinity)
+            Text("Splash").foregroundStyle(.white)
+        }
+    }
+}
