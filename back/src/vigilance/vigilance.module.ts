3cc9f8d36b2ac5d82c23779da7ad8c764322c589
Alex <alexmonac13@gmail.com>
2024-11-07 18:51:33 +0100

    add vigilences

--- back/src/vigilance/vigilance.module.ts
+++ back/src/vigilance/vigilance.module.ts
@@ -4,12 +4,11 @@ import { UserVigilanceTodo } from './schemas/user-vigilance-todo.schema';
 import { VigilanceTodo } from './schemas/vigilance-todo.schema';
 import { Vigilance } from './schemas/vigilance.schema';
 import { VigilanceController } from './vigilance.controller';
-import { VigilanceRepository } from './vigilance.repository';
 import { VigilanceService } from './vigilance.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Vigilance, VigilanceTodo, UserVigilanceTodo])],
   controllers: [VigilanceController],
-  providers: [VigilanceService, VigilanceRepository],
+  providers: [VigilanceService],
 })
 export class VigilanceModule {}
