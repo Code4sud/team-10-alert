4004d113c170b546dd50e9754ac6f222e89594e0
Alex <alexmonac13@gmail.com>
2024-11-07 14:07:54 +0100

    fix backend

--- back/src/players/players.module.ts
+++ back/src/players/players.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PlayersService } from './players.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayersController } from './players.controller';
+import { PlayersRepository } from './players.repository';
+import { Player } from './players.schema';
+import { PlayersService } from './players.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Player])],
   controllers: [PlayersController],
-  providers: [PlayersService],
+  providers: [PlayersService, PlayersRepository],
+  exports: [PlayersRepository],
 })
 export class PlayersModule {}
--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EncryptionModule } from 'src/encryption/encryption.module';
-import { Player } from './schemas/players.schema';
+import { PlayersModule } from 'src/players/players.module';
 import { User } from './schemas/users.schema';
 import { UsersController } from './users.controller';
+import { UsersMapper } from './users.mapper';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Player]), EncryptionModule],
-  providers: [UsersService, UsersRepository],
+  imports: [TypeOrmModule.forFeature([User]), EncryptionModule, PlayersModule],
   controllers: [UsersController],
+  providers: [UsersService, UsersRepository, UsersMapper],
   exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
