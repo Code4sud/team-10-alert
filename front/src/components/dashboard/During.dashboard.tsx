a3d6c87c67119132db63632b5a38de97ef4c5f82
SRBL <aleeexis.1995@icloud.com>
2024-11-07 13:46:16 +0100

    add all dashboard

---
+++ front/src/components/dashboard/During.dashboard.tsx
@@ -0,0 +1,10 @@
+const DashboardDuring = () => {
+  return (
+    <div className='text-white'>
+      <h1>Dashboard During</h1>
+      <p>Cette page permet de gérer l'alerte pendant l'alerte.</p>
+    </div>
+  );
+};
+
+export default DashboardDuring;
