c7887893d20efcca4a6df11765c6a54a83df1f9c
JohnnyJohnnyJohn <JohnnyJohnnyJohn@github.com>
2024-11-07 23:47:37 +0100

    fix width dashboard

--- front/src/screens/unauth/home.screen.tsx
+++ front/src/screens/unauth/home.screen.tsx
@@ -1,6 +1,7 @@
 import UserRequests from "@/store/users/user.request";
 import { useUserStore } from "@/store/users/user.store";
 import { useState } from "react";
+import {Button} from "@/components/ui/button";
 
 export const HomeScreen = () => {
   const { login } = useUserStore();
@@ -17,11 +18,12 @@ export const HomeScreen = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col justify-evenly items-center h-[400px] w-[50%] space-y-4 bg-black rounded-md shadow-[0px_4px_15px_rgba(100,100,100,0.5)]">
-        <h1 className="text-white text-2xl font-bold">Bienvenue sur le site de 4Sud !</h1>
+      <div className="flex flex-col justify-evenly gap-4 items-center px-8 pb-8 bg-black rounded-md shadow-[0px_4px_15px_rgba(100,100,100,0.5)]">
+        <img src="src/assets/logo.png" alt="logo" className="h-40" />
         <div className="flex flex-col items-start gap-2">
-          <label className="text-gray-500 text-sm text-end">Entrez votre code d&apos;accès</label>
+          <label htmlFor="email" className="text-white text-sm">Entrez votre email</label>
           <input name="email" type="text" placeholder="Email" value={loginData.email} onChange={handleChange} className="px-4 py-2 border rounded-md" />
+          <label htmlFor="password" className="text-white text-sm">Entrez votre mot de passe</label>
           <input
             name="password"
             type="password"
@@ -31,9 +33,9 @@ export const HomeScreen = () => {
             className="px-4 py-2 border rounded-md"
           />
         </div>
-        <button onClick={handleSubmit} className="mt-2 bg-blue-500 text-md text-white px-8 py-2 rounded-md">
+        <Button variant={"secondary"} onClick={handleSubmit} >
           Entrer
-        </button>
+        </Button>
       </div>
     </div>
   );
