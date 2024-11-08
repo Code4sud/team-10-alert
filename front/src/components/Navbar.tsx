abd7755014ccbd3dca8e0326a8d1376c1c57cb62
Fatima El mouhine <91740491+fatima-elmouhine@users.noreply.github.com>
2024-11-08 21:46:05 +0100

    change image path

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -9,20 +9,20 @@ const Navbar = () => {
     return (
         <nav
             className='bg-[#203D4E] min-h-[calc(100vh-112px)] flex flex-col items-center gap-4 min-w-48 rounded-xl pb-8 fixed'>
-            <img src='../../public/logo.png' alt='logo' className='w-full'/>
+            <img src='/logo.png' alt='logo' className='w-full'/>
             <div className='flex flex-col items-center gap-4 h-100'>
                 <NavLink
                     to='/'
                     className={({isActive}) => isActive ? 'text-[#00D9FF] font-bold flex items-center gap-4' : 'text-white flex items-center gap-4'}
                 >
-                    <img src='../../public/dashboard.svg' alt='logo' className='w-5 h-5'/>
+                    <img src='/dashboard.svg' alt='logo' className='w-5 h-5'/>
                     Dashboard
                 </NavLink>
                 <NavLink
                     to='/scenarios'
                     className={({isActive}) => isActive ? 'text-[#00D9FF] font-bold flex items-center gap-4' : 'text-white flex items-center gap-4'}
                 >
-                    <img src='../../public/scenario.svg' alt='logo' className='w-5 h-5'/>
+                    <img src='/scenario.svg' alt='logo' className='w-5 h-5'/>
                     Scénarios
                 </NavLink>
 
